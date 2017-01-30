71210a761440ac112373d5a2e6f19f13bd5f45ad
Vishal Bisht <vishalbisht7201@gmail.com>
2017-01-30 09:25:45 +0000

    lexicon files will be updated on adding a new same as intent

--- webserver/routes/getReply/reply.js
+++ webserver/routes/getReply/reply.js
@@ -6,30 +6,28 @@ let getQuestionResponse = require('./functions/getQuestionResponse');
 // let saveUnansweredQuery = require('./saveUnansweredQuery');
 router.post('/askQuestion', function(req, res) {
     let question = req.body.question;
-    let query = processQuestion(question.value);
+    let query = processQuestion(question.value.toLowerCase());
     let keywords = query.keywords;
     let intents = query.intents;
     let questionResultCallback = function(finalResult) {
-      res.json({
-          answer: finalResult.textAnswer,
-          result: finalResult.otherResult
-      });
+        res.json({
+            answer: finalResult.textAnswer,
+            result: finalResult.otherResult
+        });
+    }
+    let noAnswerFoundCallback = function() {
+        res.json({
+            answer: "Here is what i have for you",
+            keywords: keywords
+        });
     }
     if (keywords.length === 0) {
-        res.send('No keywords found');
-    } else if (intents.length === 0) {
-        // getKeywordResponse(resultCallback);
-        // saveUnansweredQuery();
+        res.json({
+            answer: 'I am not able to understand you'
+        });
     } else {
-        getQuestionResponse(intents, keywords, questionResultCallback);
+        getQuestionResponse(intents, keywords, questionResultCallback, noAnswerFoundCallback);
     }
-})
-
-
-
-
-
-
-
+});
 
 module.exports = router;
