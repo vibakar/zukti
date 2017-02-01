9844cf8d717cc6876c16e5d63f43b5402f096463
Vishal Bisht <vishalbisht7201@gmail.com>
2017-02-01 04:04:55 +0000

    added no answe reply in a json file

--- webclient/ginniAdmin/components/buildAi/questionSetDisplay.jsx
+++ webclient/ginniAdmin/components/buildAi/questionSetDisplay.jsx
@@ -1,10 +1,10 @@
+/*
 import React from 'react';
 import {
     Divider,
     Grid,
     Dropdown,
     Input,
-    Icon,
     Radio,
     Button
   } from 'semantic-ui-react';
@@ -98,6 +98,59 @@ export default class QuestionSetDisplay extends React.Component {
                       <Grid.Column>
                       <Button color='teal'>ADD QUERY</Button>
                     </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+        );
+    }
+}
+*/
+import React from 'react';
+import {
+    Divider,
+    Grid
+  } from 'semantic-ui-react';
+import QuestionAnswer from './questionsAnswer';
+import AddQuestionAnswerSet from './addQuestionsAnswerSet';
+
+export default class QuestionSetDisplay extends React.Component {
+
+    constructor() {
+        super();
+        this.addQuestionAnswerSet = this.addQuestionAnswerSet.bind(this);
+        this.state = {
+           questionAnswerSet: []
+        };
+    }
+    // function to add a Question answer set to display
+    addQuestionAnswerSet(id) {
+        this.state.questionAnswerSet.push(<QuestionAnswer answerID={id}/>);
+        this.setState({questionAnswerSet: this.state.questionAnswerSet});
+    }
+    render() {
+        return (
+            <div style={{
+                backgroundImage: "url('../../images/wall.jpg')"
+            }}>
+                <Grid style={{
+                    width: '95%',
+                    margin: 'auto'
+                }}>
+                    <Grid.Row columns={1}>
+                    </Grid.Row>
+                    <Grid.Row columns={1}>
+                        <div style={{
+                            width: '100%'
+                        }}>
+                            <p>It is a set of questions and the corresponding bot replies. Ginni will understand user questions similar to those you’ve set up and reply with a appropiate answer.</p>
+                            <Divider fitted/>
+                        </div>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <AddQuestionAnswerSet handlerAddQASet={this.addQuestionAnswerSet}/>
+                    </Grid.Row>
+                    <Grid.Row>
+                      {this.state.questionAnswerSet}
                     </Grid.Row>
                 </Grid>
             </div>
--- webserver/neo4j/connection.js
+++ webserver/neo4j/connection.js
@@ -1,7 +1,5 @@
+let neo4j = require('neo4j-driver').v1;
 module.exports = function() {
-    let neo4j = require('neo4j-driver').v1;
-
-    let driver = neo4j.driver("bolt://192.168.1.102", neo4j.auth.basic("neo4j", "neo4js"));
-
+    let driver = neo4j.driver("bolt://192.168.0.35", neo4j.auth.basic("neo4j", "Wilkinson"));
     return driver;
 };
