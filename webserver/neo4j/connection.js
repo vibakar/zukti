3ccfc88a9fc362ac838194ddfcc8042c42d5a483
Vishal Bisht <vishalbisht7201@gmail.com>
2017-02-07 10:24:02 +0530

    connection.js modified

--- webserver/neo4j/connection.js
+++ webserver/neo4j/connection.js
@@ -1,6 +1,5 @@
 let neo4j = require('neo4j-driver').v1;
 module.exports = function() {
-
     let driver = neo4j.driver("bolt://192.168.1.37", neo4j.auth.basic("neo4j", "Wilkinson"));
     return driver;
 };
